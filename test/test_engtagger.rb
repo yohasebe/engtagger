f62a7f0b3cecbb368a8e97f8946116745609cdae
Dorian Taylor <code@doriantaylor.com>
2020-08-05 11:48:19 -0700

    added tag_pairs method

--- test/test_engtagger.rb
+++ test/test_engtagger.rb
@@ -7,16 +7,24 @@ $LOAD_PATH << $ENGTAGGER_LIB
 require 'test/unit' unless defined? $ZENTEST and $ZENTEST
 require 'engtagger'
 
+# copy the private methods into public methods beginning with `t_`
+class EngTagger
+  %i[classify_unknown_word clean_word get_max_noun_regex reset
+    split_punct split_sentences strip_tags valid_text].each do |method|
+    define_method "t_#{method}".to_sym, instance_method(method)
+  end
+end
+
 class TestEngTagger < Test::Unit::TestCase
-  
+
   @@untagged =<<EOD
-Lisa Raines, a lawyer and director of government relations for the Industrial Biotechnical Association, contends that a judge well-versed in patent law and the concerns of research-based industries would have ruled otherwise. And Judge Newman, a former patent lawyer, wrote in her dissent when the court denied a motion for a rehearing of the case by the full court, "The panel's judicial legislation has affected an important high-technological industry, without regard to the consequences for research and innovation or the public interest." Says Ms. Raines, "[The judgement] confirms our concern that the absence of patent lawyers on the court could prove troublesome."    
+Lisa Raines, a lawyer and director of government relations for the Industrial Biotechnical Association, contends that a judge well-versed in patent law and the concerns of research-based industries would have ruled otherwise. And Judge Newman, a former patent lawyer, wrote in her dissent when the court denied a motion for a rehearing of the case by the full court, "The panel's judicial legislation has affected an important high-technological industry, without regard to the consequences for research and innovation or the public interest." Says Ms. Raines, "[The judgement] confirms our concern that the absence of patent lawyers on the court could prove troublesome."
 EOD
 
   @@tagged =<<EOD
 <nnp>Lisa</nnp> <nnp>Raines</nnp> <ppc>,</ppc> <det>a</det> <nn>lawyer</nn> <cc>and</cc> <nn>director</nn> <in>of</in> <nn>government</nn> <nns>relations</nns> <in>for</in> <det>the</det> <nnp>Industrial</nnp> <nnp>Biotechnical</nnp> <nnp>Association</nnp> <ppc>,</ppc> <vbz>contends</vbz> <in>that</in> <det>a</det> <nn>judge</nn> <jj>well-versed</jj> <in>in</in> <nn>patent</nn> <nn>law</nn> <cc>and</cc> <det>the</det> <nns>concerns</nns> <in>of</in> <jj>research-based</jj> <nns>industries</nns> <md>would</md> <vb>have</vb> <vbn>ruled</vbn> <rb>otherwise</rb> <pp>.</pp>
 EOD
-  
+
   def setup
     @tagger = EngTagger.new
     tagpath = File.join($ENGTAGGER_LIB, @tagger.conf[:tag_path])
@@ -25,17 +33,17 @@ EOD
       @tagger.install
     end
   end
-  
+
   def text_get_ext
     model = '<cd>[^<]+</cd}>\s*'
     assert_equal(model, EngTagger.get_ext(model, "cd"))
   end
-  
+
   def test_explain_tag
     assert_equal("noun", EngTagger.explain_tag("nn"))
-    assert_equal("verb_infinitive", EngTagger.explain_tag("vb"))  
+    assert_equal("verb_infinitive", EngTagger.explain_tag("vb"))
   end
-  
+
   def test_add_tags
     assert_instance_of(String, @tagger.add_tags(@@untagged))
   end
@@ -54,16 +62,16 @@ EOD
       assert(EngTagger.hmm.keys.index(result))
     end
   end
-     
+
   def test_classify_unknown_word
-    assert_equal("*LRB*", @tagger.classify_unknown_word("{"))
-    assert_equal("*NUM*", @tagger.classify_unknown_word("123.4567"))
-    assert_equal("*ORD*", @tagger.classify_unknown_word("40th"))
-    assert_equal("-abr-", @tagger.classify_unknown_word("GT-R"))
-    assert_equal("-hyp-adj-", @tagger.classify_unknown_word("extremely-high"))
-    assert_equal("-sym-", @tagger.classify_unknown_word("&&"))
-    assert_equal("-ing-", @tagger.classify_unknown_word("wikiing"))
-    assert_equal("-unknown-", @tagger.classify_unknown_word("asefasdf"))
+    assert_equal("*LRB*", @tagger.t_classify_unknown_word("{"))
+    assert_equal("*NUM*", @tagger.t_classify_unknown_word("123.4567"))
+    assert_equal("*ORD*", @tagger.t_classify_unknown_word("40th"))
+    assert_equal("-abr-", @tagger.t_classify_unknown_word("GT-R"))
+    assert_equal("-hyp-adj-", @tagger.t_classify_unknown_word("extremely-high"))
+    assert_equal("-sym-", @tagger.t_classify_unknown_word("&&"))
+    assert_equal("-ing-", @tagger.t_classify_unknown_word("wikiing"))
+    assert_equal("-unknown-", @tagger.t_classify_unknown_word("asefasdf"))
   end
 
   def test_clean_text
@@ -79,7 +87,7 @@ EOD
     models += ["pleadingly"]
     tests += ["1973.0820", "Plays", "Pleadingly"]
     models.length.times do |i|
-      assert_equal(models[i], @tagger.clean_word(tests[i]))
+      assert_equal(models[i], @tagger.t_clean_word(tests[i]))
     end
   end
 
@@ -89,10 +97,10 @@ EOD
   end
 
   def test_get_max_noun_regex
-    assert_instance_of(Regexp, @tagger.get_max_noun_regex)
+    assert_instance_of(Regexp, @tagger.t_get_max_noun_regex)
   end
 
-  def test_get_noun_phrases    
+  def test_get_noun_phrases
     result = @tagger.get_noun_phrases(@@tagged)
     assert_instance_of(Hash, result)
   end
@@ -162,7 +170,7 @@ EOD
 
   def test_reset
     @tagger.conf[:current_tag] = 'nn'
-    @tagger.reset
+    @tagger.t_reset
     assert_equal('pp', @tagger.conf[:current_tag])
   end
 
@@ -182,10 +190,10 @@ EOD
     models << ["test", "#", "test"]; texts <<  "test#test"
     models << ["I", "'d", "like"]; texts <<  "I'd like"
     models << ["is", "n't", "so"]; texts <<  "isn't so"
-    models << ["we", "'re", "all"]; texts <<  "we're all"  
+    models << ["we", "'re", "all"]; texts <<  "we're all"
 
     texts.each_with_index do |text, index|
-      assert_equal(models[index], @tagger.split_punct(text))
+      assert_equal(models[index], @tagger.t_split_punct(text))
     end
   end
 
@@ -194,9 +202,9 @@ EOD
     models << ["He", "is", "a", "u.s.", "army", "officer", "."]
     tests << ["He", "is", "a", "u.s.", "army", "officer."]
     models << ["He", "is", "Mr.", "Johnson", ".", "He", "'s", "my", "friend", "."]
-    tests << ["He", "is", "Mr.", "Johnson.", "He", "'s", "my", "friend."]    
+    tests << ["He", "is", "Mr.", "Johnson.", "He", "'s", "my", "friend."]
     models.length.times do |i|
-      assert_equal(models[i], @tagger.split_sentences(tests[i]))
+      assert_equal(models[i], @tagger.t_split_sentences(tests[i]))
     end
   end
 
@@ -206,24 +214,23 @@ EOD
     @tagger.conf[:stem] = true
     assert_equal("get", @tagger.stem(word))
     # the following should not work since we memoize stem method
-    # @tagger.conf[:stem] = false    
+    # @tagger.conf[:stem] = false
     # assert_equal("gets", @tagger.stem(word))
     @tagger.conf[:stem] = old
   end
 
   def test_strip_tags
-    assert_instance_of(String, @tagger.strip_tags(@@tagged))
+    assert_instance_of(String, @tagger.t_strip_tags(@@tagged))
   end
 
   def test_valid_text
     text = nil
-    assert(!@tagger.valid_text(text))
+    assert(!@tagger.t_valid_text(text))
     text = "this is test text"
-    assert(@tagger.valid_text(text))
+    assert(@tagger.t_valid_text(text))
     text = ""
-    assert(!@tagger.valid_text(text))
+    assert(!@tagger.t_valid_text(text))
   end
-  
   def test_override_default_params
     @tagger = EngTagger.new(:longest_noun_phrase => 3)
     assert_equal 3, @tagger.conf[:longest_noun_phrase]
